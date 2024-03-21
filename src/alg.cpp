8248d935d4fd3b44096b0054e6e1d1e6ff35e6e7
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-03-21 16:13:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,14 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  int k = 0;
+  for (int i = 0; arr[i] <= value; i++)
+  {
+    if (arr[i] == value)
+      k++;
+  }
+  if(k > 0)
+  return k;
+  else
   return 0; // если ничего не найдено
 }
